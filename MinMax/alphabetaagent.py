07532c1454a2c8cf3fcf46ee88034365f5fcbe0e
Patryk Miszke <131891673+Pamisz@users.noreply.github.com>
2024-04-20 23:19:35 +0200

    Update alphabetaagent.py

--- MinMax/alphabetaagent.py
+++ MinMax/alphabetaagent.py
@@ -1,4 +1,6 @@
 import math
+from copy import deepcopy
+
 
 class AlphaBetaAgent:
     def __init__(self, token, depth=3):
@@ -7,64 +9,88 @@ class AlphaBetaAgent:
 
     def decide(self, game):
         possible_moves = game.possible_drops()
-        best_move = None
+        best_move = -1
         alpha = -math.inf
         beta = math.inf
+        best_score = -math.inf
 
         for move in possible_moves:
-            game_copy = game.copy()
+            game_copy = deepcopy(game)
             game_copy.drop_token(move)
-            score = self.alphabeta(game_copy, self.depth, alpha, beta, maximizing_player=False if self.token == 'x' else True)
+            score = self.alphabeta(game_copy, self.depth - 1, alpha, beta, 0)
 
-            if (self.token == 'x' and score > alpha) or (self.token == 'o' and score < beta):
-                alpha = score
+            if score > best_score:
+                best_score = score
                 best_move = move
-
+            alpha = max(alpha, best_score)
         return best_move
 
     def alphabeta(self, game, depth, alpha, beta, maximizing_player):
-        if depth == 0 or game._check_game_over():
+        if game._check_game_over():
+            if game.wins == self.token:
+                return 1
+            elif game.wins is None:
+                return 0
+            else:
+                return -1
+
+        if depth == 0:
             return self.evaluate(game)
 
-        if maximizing_player:
-            v = -math.inf
-            for move in game.possible_drops():
-                game_copy = game.copy()
-                game_copy.drop_token(move)
-                v = max(v, self.alphabeta(game_copy, depth - 1, alpha, beta, maximizing_player=False))
+        v = -math.inf if maximizing_player else math.inf
+        for move in game.possible_drops():
+            game_copy = deepcopy(game)
+            game_copy.drop_token(move)
+            if maximizing_player:
+                v = max(v, self.alphabeta(game_copy, depth - 1, alpha, beta, 0))
                 alpha = max(alpha, v)
                 if v >= beta:
-                    break  # β cutoff
-            return v
-        else:
-            v = math.inf
-            for move in game.possible_drops():
-                game_copy = game.copy()
-                game_copy.drop_token(move)
-                v = min(v, self.alphabeta(game_copy, depth - 1, alpha, beta, maximizing_player=True))
+                    break
+            else:
+                v = min(v, self.alphabeta(game_copy, depth - 1, alpha, beta, 1))
                 beta = min(beta, v)
                 if v <= alpha:
-                    break  # α cutoff
-            return v
+                    break
+
+        return v
 
     def evaluate(self, game):
         score = 0
-        for four in game.iter_fours():
-            if four.count(self.token) == 4:  # Gracz wygrywa
-                score += 1000
-            elif four.count(self.token) == 3 and four.count('_') == 1:  # Gracz ma trzy w rzędzie z jednym wolnym miejscem
-                score += 100
-            elif four.count(self.token) == 2 and four.count('_') == 2:  # Gracz ma dwie w rzędzie z dwoma wolnymi miejscami
-                score += 10
-            elif four.count(self.token) == 1 and four.count('_') == 3:  # Gracz ma jedną w rzędzie z trzema wolnymi miejscami
-                score += 1
+        board = game.center_column()
+        if self.token == "x":
+            enemy_token = "o"
+        else:
+            enemy_token = "x"
 
-            if four.count('x' if self.token == 'o' else 'o') == 4:  # Przeciwnik wygrywa
-                score -= 1000
-            elif four.count('x' if self.token == 'o' else 'o') == 3 and four.count('_') == 1:  # Przeciwnik ma trzy w rzędzie z jednym wolnym miejscem
-                score -= 100
-            elif four.count('x' if self.token == 'o' else 'o') == 2 and four.count('_') == 2:  # Przeciwnik ma dwie w rzędzie z dwoma wolnymi miejscami
-                score -= 10
-            elif four.count('x' if self.token == 'o' else 'o') == 1 and four.count('_') == 3:  # Przeciwnik ma jedną w rzędzie z trzema wolnymi miejscami
-                score -= 1
+        for i in range(len(board)):
+            if board[i] == self.token:
+                score += 0.1
+            elif board[i] == enemy_token:
+                score -= 0.1
+
+        for four in game.iter_fours():
+            if four.count(self.token) == 4:
+                score = 1
+                break
+            elif four.count(self.token) == 3 and four.count('_') == 1:
+                score += 0.2
+            elif four.count(self.token) == 2 and four.count(
+                    '_') == 2:
+                score += 0.1
+            elif four.count(self.token) == 1 and four.count(
+                    '_') == 3:
+                score += 0.01
+
+            elif four.count(enemy_token) == 4:
+                score = -1
+                break
+            elif four.count(enemy_token) == 3 and four.count(
+                    '_') == 1:
+                score -= 0.2
+            elif four.count(enemy_token) == 2 and four.count(
+                    '_') == 2:
+                score -= 0.1
+            elif four.count(enemy_token) == 1 and four.count(
+                    '_') == 3:
+                score -= 0.01
         return score
